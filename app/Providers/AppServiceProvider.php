c2770e5ad43aeb04c4e08776a170427e1af4ef12
root <root@192.168.56.128>
2018-12-18 11:55:48 +0800

    init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Antvel e-commerce.
+ *
+ * (c) Gustavo Ocanto <gustavoocanto@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Providers;
+
+use Illuminate\Pagination\Paginator;
+use Laravel\Dusk\DuskServiceProvider;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Paginator::useBootstrapThree();
+    }
+
+    /**
+     * Register any application services.
+     *
+     * This service provider is a great spot to register your various container
+     * bindings with the application. As you can see, we are registering our
+     * "Registrar" implementation here. You can add your own bindings too!
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
+    }
+}
